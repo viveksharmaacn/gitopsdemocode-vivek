aa313ce9cf1a939b1731b5c087dfe9d7c133b99e
rakhar <rakhi.parashar@accenture.com>
2023-03-09 13:00:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello GitOps - Rakhi Parashar'
-
-if __name__ == "__main__":
-    app.run(debug=True)
+app = Flask(__name__) @app.route("/")
+def hello():
+    return "Hello World - Rakhi!"
